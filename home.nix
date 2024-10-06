163edc343b9862558e71c4cac22ca98fc7106727
Drin Emini <Drinemini@outlook.de>
2024-10-06 20:26:29 +0200

    Home.nix needs to be added manually?

--- home.nix
+++ home.nix
@@ -2,22 +2,23 @@
 
 {
 	imports = [ 
-    		./modules/default.nix
+    	./modules/default.nix
 	];
+
 	home.username = "vol";
 	home.homeDirectory = "/home/vol";
 
-  xdg.userDirs = {
-	  enable = true;
-	  createDirectories = true;
-	  desktop = null;
-	  download = "${config.home.homeDirectory}/DOWNLOADS";
-	  pictures = "${config.home.homeDirectory}/PICS";
-	  documents = null;
-	  music = null;
-	  videos = null;
-	  publicShare = null;
-	  templates = null;
+    xdg.userDirs = {
+	    enable = true;
+	    createDirectories = true;
+	    desktop = null;
+	    download = "${config.home.homeDirectory}/DOWNLOADS";
+	    pictures = "${config.home.homeDirectory}/PICS";
+	    documents = null;
+	    music = null;
+	    videos = null;
+	    publicShare = null;
+	    templates = null;
 	};
 
 	home.stateVersion = "24.05";
@@ -25,25 +26,27 @@
 	fonts.fontconfig.enable = true;
 
 	home.packages = [
-    	  (pkgs.nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" ]; })
+    	(pkgs.nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" ]; })
 	];
   
 	home.file.".i3" = {
-    source = ./dotfiles/i3;
-  	onChange = ''${pkgs.i3}/bin/i3-msg reload'';
-  };
-	
-  programs.alacritty = {
-    enable = true;
-    settings.window.padding = {
-      x = 8;
-      y = 8; 
+        source = ./dotfiles/i3;
+  	    onChange = ''${pkgs.i3}/bin/i3-msg reload'';
     };
-  };
 
-	programs.firefox.enable = true;
+    home.file.".config/neofetch".source = ./dotfiles/neofetch;
+	
+    programs.alacritty = {
+        enable = true;
+        settings.window.padding = {
+            x = 8;
+            y = 8; 
+        };
+    };
+
+    programs.firefox.enable = true;
 	programs.rofi.enable = true;
-  programs.obs-studio.enable = true;
+    programs.obs-studio.enable = true;
 
 	home.sessionVariables = {};
 
